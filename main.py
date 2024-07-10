4ece0a32d942e4aad4e9ed72187740fb2bcddde4
devyani <devyanibrd123@gmail.com>
2024-07-10 19:54:10 +0530

    updated

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from flask import Flask,request
 from flask import render_template,redirect
 from flask_sqlalchemy import SQLAlchemy
 import DateTime
+import os
+from flask_mail import Mail,Message
 
 app=Flask(__name__)
+
 
 # configure the SQLite database, relative to the app instance folder
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///myfirstwebdb.db"
@@ -11,12 +14,22 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///myfirstwebdb.db"
 #db.init_app(app)
 db=SQLAlchemy(app)
 
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT']=465
+app.config['MAIL_USERNAME']="devyanibrd123@gmail.com"
+app.config['MAIL_PASSWORD']="hhjt lrkr fuvq ahan"
+app.config['MAIL_USE_SSL']=True
+app.config['MAIL_USE_TLS ']=False
+
+mail=Mail(app)
+
 class contactus(db.Model):
     id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     username = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String)
     phn_no=db.Column(db.Text)
     message=db.Column(db.Text)
+    img_path=db.Column(db.String)
     date=db.Column(db.DateTime,server_default=db.func.now())
 
   
@@ -54,17 +67,27 @@ def service():
 @app.route("/savethisdata",methods=["POST"])
 def datasaving():
     if request.method=="POST":
-        username=request.form.get("fname")
+        usernasme=request.form.get("fname")
         email=request.form.get("email")
         
         phone=request.form.get("phn")
         message=request.form.get("msg")
+        image=request.files.get("img")
+
+        if image:
+            image.save(os.path.join("static/userimg",image.filename))
+            path=os.path.join("static/userimg",image.filename)
+
         # return f"username //is {username},user email:{email}:{email},user phn no is:{phone},and user message is:{message}"
     
-        page=contactus( username=  username,email=email,phn_no= phone,message=message)
+        page=contactus(username= usernasme,email=email,phn_no= phone,message=message,img_path=path)
 
         db.session.add(page)
         db.session.commit()
+
+        msg=Message("testing", sender="devyanibrd123@gmail.com", recipients=["devanshscience@gmail.com"])
+        msg.body = "HELLO DEVANSH"
+        mail.send(msg)
 
         return redirect("/service")
 
@@ -102,6 +125,7 @@ def updatethisu(abc):
 
             db.session.commit()
             return redirect("/service")
+        
 
 
         #return render_template("pages/updatedata.html",data=data)
